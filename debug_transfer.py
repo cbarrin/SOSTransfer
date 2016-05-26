a4d168259d1354f14484bc55984e2ec1e94d2ad6
Geddings <cbarrin@g.clemson.edu>
2016-05-26 16:26:03 -0400

    Testing out dpkt module to parse pcap files

--- debug_transfer.py
+++ debug_transfer.py
@@ -1,1 +1,14 @@
 # Python script to debug transfers using SOS
+# cat /proc/meminfo
+# cat /proc/loadavg
+
+import dpkt
+
+f = open('test.pcap')
+pcap = dpkt.pcap.Reader(f)
+
+for ts, buf in pcap:
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+    print tcp.win
