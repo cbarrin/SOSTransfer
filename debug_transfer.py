2fd91642ce6cb80124e3affdf7061b1cc49af503
Geddings <cbarrin@g.clemson.edu>
2016-05-26 16:39:15 -0400

    Testing out dpkt module to parse pcap files

--- debug_transfer.py
+++ debug_transfer.py
@@ -5,12 +5,20 @@
 import dpkt
 import socket
 
+import plotly.plotly as py
+import plotly.graph_objs as go
+
 f = open('test.pcap')
 pcap = dpkt.pcap.Reader(f)
 
+windows = []
 for ts, buf in pcap:
     eth = dpkt.ethernet.Ethernet(buf)
     ip = eth.data
     tcp = ip.data
     if socket.inet_ntoa(ip.src) == "10.0.0.3":
-        print(tcp.win)
+        windows.append(tcp.win)
+        #print(tcp.win)
+
+print windows
+
